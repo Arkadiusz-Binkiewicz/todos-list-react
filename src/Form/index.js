eab322e53d882a8c5d9d15645fee9dde2c5484a5
Arkadiusz Binkiewicz <arek.binkiewicz11@onet.pl>
2023-05-23 20:13:45 +0200

    semicolon added

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,7 +1,7 @@
-import "./style.css"
+import "./style.css";
 
 const Form = () => (
-    <form className="form">
+  <form className="form">
     <input className="form__input" placeholder="Co jest do zrobienia?"/>
     <button className="form__button">Dodaj zadanie</button>
   </form>
